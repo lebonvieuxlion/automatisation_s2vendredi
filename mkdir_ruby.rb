d7f22f7ebf4dda4fbbc1cbac2dc965b3df90a2bb
lebonvieuxlion <debaugelion@gmail.com>
2019-01-19 01:18:54 +0100

    Exo 2 réalisé, initiation projet ruby automatisée

--- mkdir_ruby.rb
+++ mkdir_ruby.rb
@@ -2,11 +2,14 @@ require 'fileutils'
 require 'git'
 require 'rubygems'
 
+
+
 #CHECK SI L'UTILISATEUR A BIEN MIS UN NOM DE DOSSIER
 
 def check_if_user_gave_input
   abort("mkdir: missing input") if ARGV.empty? 		#la fonction abort permet d'interrompre le programme si la condition if est remplie. En l'occurence
 end													#si ARGV est vide
+
 
 
 # RECUPERE LE NOM DU DOSSIER
@@ -15,11 +18,14 @@ def get_folder_name
   return folder_name = ARGV.first					#permet de récupérer uniquement le premier élément du ARGV (qui a la classe array)
 end
 
+
+
 # CREE LE DOSSIER
 
 def create_folder(name)
   Dir.mkdir(name)									
 end
+
 
 
 #CHECK LE NOM DE DOSSIER ENTRE, RECUPERE LE NOM ET LE CREE
@@ -43,17 +49,13 @@ def gemfile_setup
 	file.puts("gem 'dotenv'")
 
 	file.close
-end
-
-
-
-#DEPLACEMENT DU GEMFILE
-
-def move_gemfile
 
 	path_name = get_folder_name
 	FileUtils.mv "Gemfile", "#{path_name}/Gemfile"		#permet de bouger le fichier Gemfile dans le dossier créé
+
 end
+
+
 
 
 #INITIALISATION DE GIT
@@ -87,7 +89,7 @@ def creation_lib_directory
 end
 
 
-#CREATION README
+#CREATION FICHIER README
 
 def creation_readme
 
@@ -97,7 +99,33 @@ def creation_readme
 end
 
 
-create_dir
-creation_readme
 
-#Manque juste à faire le rspec --init 
+
+#INIT RESPECT AVEC LA CREATION DU DOSSIER SPEC ET DU FICHIER .RSPEC
+
+def init_rspec
+
+	system("rspec --init")						  #exécution des scripts entre "" dans le terminal
+	system("mv spec #{get_folder_name}")
+	system("mv .rspec #{get_folder_name}")
+
+end
+
+
+
+#METHODE FINALE REGROUPANT TOUTES LES AUTRES ET SERVANT A INITIER UN PROJET EN RUBY FACILEMENT
+
+
+def perform
+
+	create_dir
+	gemfile_setup
+	initializing_git
+	creating_env_and_gitignore
+	creation_lib_directory
+	init_rspec
+	creation_readme
+
+end
+
+perform
